af86916b4e33136d1bd974aec89c1d217843e428
dima <giomozda74@gmail.com>
2025-04-22 23:46:57 +0300

    add required parametr to MovieSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -5,8 +5,8 @@ from cinema.models import Movie
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=True)
-    description = serializers.CharField(max_length=255)
+    title = serializers.CharField(max_length=255, required=True)
+    description = serializers.CharField(max_length=255, required=True)
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
